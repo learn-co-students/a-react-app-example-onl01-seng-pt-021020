be2e6e91d19933d38770fb3aa56afca8f84b1036
amkera <60901491+amkera@users.noreply.github.com>
2020-10-07 12:56:44 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import ExampleComponent from './ExampleComponent'
 import TestComponent from './TestComponent'
 
 // Add your code own within the return statement
+//You can see in the App class that Component and moment are both being used in the render() method.
+//They are being imported from the node_modules folder.
 
 class App extends Component {
+  // all of the visible content of our app is contained within this App class.
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          <h1>Now</h1>
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
@@ -19,6 +22,7 @@ class App extends Component {
           allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
         <ExampleComponent />
+        <TestComponent />
 
       </div>
     );
@@ -26,3 +30,5 @@ class App extends Component {
 }
 
 export default App;
+
+//By including the export line, we are allowing other files to import things from the App.js file.
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//default
+//importing App from App.js (the .js is not included, but still implied).
+//This is the syntax to import something that is the default export of another file:
 
 ReactDOM.render(<App />, document.getElementById('root'));
